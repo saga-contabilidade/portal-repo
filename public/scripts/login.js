1e86cfea456bd8925247114e02da5ee4d1ceb1e0
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-07-11 18:03:25 -0300

    Update: dinamic html

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,22 +1,49 @@
-// ================================
-// SagaHub - Script de Login Simples
-// ================================
-
-// Seleciona elementos do DOM
 const form = document.getElementById('login-form');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const errorMessage = document.getElementById('error-message');
+const quotesBox = document.querySelector('.quote-box');
+const authorText = document.querySelector('.author');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Previne recarregamento da página
+  event.preventDefault();
+ // 1. Mock de objetos com as citações e autores, futuramente gerado automaticamente
+  const quotes = [
+    {
+      text: "Eu posso aceitar o fracasso. Todo mundo falha em alguma coisa. Mas eu não posso aceitar não tentar.",
+      author: "Jordan, Michael."
+    },
+    {
+      text: "A persistência é o caminho do êxito.",
+      author: "Chaplin, Charles."
+    },
+    {
+      text: "O único lugar onde o sucesso vem antes do trabalho é no dicionário.",
+      author: "Einstein, Albert."
+    },
+    {
+        text: "Tente de novo. Fracasse de novo. Mas fracasse melhor.",
+        author: "Beckett, Samuel."
+    },
+    {
+        text: "Sua única limitação é você mesmo.",
+        author: "Anônimo"
+    } 
+  ]
+
+  if (quotesBox && authorText){
+    const randomIdex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIdex];
+    quotesBox.innerHTML = `<blockquote>"${randomQuote.text}”</blockquote>`;
+
+    authorText.innerHTML = `- ${randomQuote.author}`;
+  }
 
   const email = emailInput.value.trim();
   const password = passwordInput.value;
 
-  errorMessage.textContent = ''; // Limpa mensagens anteriores
+  errorMessage.textContent = '';
 
-  // Validação básica
   if (!email.includes('@') || password.length < 3) {
     errorMessage.textContent = 'Preencha os campos corretamente.';
     console.warn('Validação falhou: email ou senha inválidos.');
